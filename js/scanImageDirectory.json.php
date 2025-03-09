0b1b99b85e24856059fbe8403519220697fa4bcd
Jim Leon <jimleon@hotmail.com>
2025-03-09 05:50:10 +0000

    Garbage collect

--- js/scanImageDirectory.json.php
+++
@@ -1,6 +0,0 @@
-
-if (!isset($_GET['callback']) || !isset($_GET['directory'])) exit;
-
-$dirContents = glob($_GET['directory'].'/*.{jpg,gif,png}', GLOB_BRACE);
-
-echo $_GET['callback'] . JSON_ENCODE($dirContents);
